fafc55cf0972aad710233e41695f73f5808d1861
Cjsah <2684117397@qq.com>
2020-11-21 19:44:00 +0800

    init

--- src/main/java/net/cjsah/cpp/mixin/MainMixin.java
+++ src/main/java/net/cjsah/cpp/mixin/MainMixin.java
@@ -9,7 +9,5 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(TitleScreen.class)
 public class MainMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
-	}
+	private void init(CallbackInfo info) {}
 }
