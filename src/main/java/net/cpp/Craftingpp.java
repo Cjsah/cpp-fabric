f7fd3305a53408397c8c42751cb581b5b5c94516
Cjsah <2684117397@qq.com>
2020-12-01 20:52:00 +0800

    change group

--- src/main/java/net/cpp/Craftingpp.java
+++ src/main/java/net/cpp/Craftingpp.java
@@ -9,24 +9,16 @@ import net.minecraft.util.Identifier;
 
 public class Craftingpp implements ModInitializer {
 
-	public static final ItemGroup CPP_GROUP = FabricItemGroupBuilder.create(new Identifier("cpp", "title")).icon(() -> new ItemStack(CppBlocks.CRAFTING_MACHINE)).build();
-
 	public static final ItemGroup CPP_GROUP_MACHINE = FabricItemGroupBuilder.create(new Identifier("cpp:machine_title")).icon(() -> new ItemStack(CppBlocks.CRAFTING_MACHINE)).build();
 	public static final ItemGroup CPP_GROUP_MISC = FabricItemGroupBuilder.create(new Identifier("cpp:misc_title")).icon(() -> new ItemStack(CppItems.ENCHANTED_IRON)).build();
 	public static final ItemGroup CPP_GROUP_TOOL = FabricItemGroupBuilder.create(new Identifier("cpp:tool_title")).icon(() -> new ItemStack(CppItems.BROOM)).build();
 	public static final ItemGroup CPP_GROUP_FOOD = FabricItemGroupBuilder.create(new Identifier("cpp:food_title")).icon(() -> new ItemStack(CppItems.CITRUS)).build();
 	public static final ItemGroup CPP_GROUP_PLANT = FabricItemGroupBuilder.create(new Identifier("cpp:plant_title")).icon(() -> new ItemStack(CppItems.ORE_SAPLING)).build();
 
-
-
-
-
-
-
 	@Override
 	public void onInitialize() {
-		CppItems.register();
 		CppBlocks.register();
+		CppItems.register();
 		CppBlockEntities.register();
 		CppScreenHandler.register();
 		CppRecipes.register();
--- src/main/java/net/cpp/init/CppBlocks.java
+++ src/main/java/net/cpp/init/CppBlocks.java
@@ -8,6 +8,9 @@ import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import static net.cpp.Craftingpp.CPP_GROUP_MACHINE;
+import static net.cpp.Craftingpp.CPP_GROUP_MISC;
+
 public final class CppBlocks {
     public static final Block CRAFTING_MACHINE = Registry.register(Registry.BLOCK, new Identifier("cpp:crafting_machine"), new CraftingMachineBlock());
     public static final Block ALL_IN_ONE_MACHINE = Registry.register(Registry.BLOCK, new Identifier("cpp:all_in_one_machine"), new CraftingMachineBlock());
@@ -21,8 +24,8 @@ public final class CppBlocks {
 
     public static void register() {
 
-        Registry.register(Registry.ITEM, new Identifier("cpp:crafting_machine"), new BlockItem(CRAFTING_MACHINE, new Item.Settings().group(Craftingpp.CPP_GROUP)));
-        Registry.register(Registry.ITEM, new Identifier("cpp:all_in_one_machine"), new BlockItem(ALL_IN_ONE_MACHINE, new Item.Settings().group(Craftingpp.CPP_GROUP)));
+        Registry.register(Registry.ITEM, new Identifier("cpp:crafting_machine"), new BlockItem(CRAFTING_MACHINE, new Item.Settings().group(CPP_GROUP_MACHINE)));
+        Registry.register(Registry.ITEM, new Identifier("cpp:all_in_one_machine"), new BlockItem(ALL_IN_ONE_MACHINE, new Item.Settings().group(CPP_GROUP_MACHINE)));
 
     }
 
