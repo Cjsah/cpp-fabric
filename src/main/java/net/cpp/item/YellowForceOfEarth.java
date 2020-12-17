ebaf4759289df5efc2bea5d7f9892697a7b4aabd
Cjsah <2684117397@qq.com>
2020-12-18 00:04:58 +0800

    黄色地之力完成

--- src/main/java/net/cpp/item/YellowForceOfEarth.java
+++ src/main/java/net/cpp/item/YellowForceOfEarth.java
@@ -1,9 +1,42 @@
 package net.cpp.item;
 
+import com.google.common.collect.ImmutableList;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class YellowForceOfEarth extends Item {
+
+    private final ImmutableList<Block> canFillFluids = ImmutableList.of(Blocks.WATER, Blocks.LAVA);
+
     public YellowForceOfEarth(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack item = user.getStackInHand(hand);
+        if (!world.isClient) {
+            user.incrementStat(Stats.USED.getOrCreateStat(this));
+            for (int i = -7; i <= 7; i++) {
+                for (int j = -2; j <= 0; j++) {
+                    for (int k = -7; k <= 7; k++) {
+                        BlockPos pos = new BlockPos(user.getPos().add(i, j, k));
+                        if (canFillFluids.contains(world.getBlockState(pos).getBlock())) {
+                            world.setBlockState(pos, Blocks.AIR.getDefaultState());
+                        }
+                    }
+                }
+            }
+            return TypedActionResult.success(item);
+        }
+        return TypedActionResult.pass(item);
     }
 }
