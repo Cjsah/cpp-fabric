b1851c52d740078eaa6dbeeff0f26168b59ec987
Cjsah <2684117397@qq.com>
2020-12-23 18:11:10 +0800

    revert了一下...

--- src/main/java/net/cpp/item/Character.java
+++ src/main/java/net/cpp/item/Character.java
@@ -1,11 +1,17 @@
 package net.cpp.item;
 
 import net.cpp.init.CppItems;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.world.World;
 
 import java.util.Random;
 
@@ -25,6 +31,17 @@ public class Character extends Item {
                 stacks.add(item);
             }
         }
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        // 还不能贴在床上
+        return ActionResult.PASS;
+    }
+
+    @Override
+    public String getTranslationKey(ItemStack stack) {
+        return super.getTranslationKey(stack) + (stack.hasTag() ? stack.getTag().getInt("character") : "");
     }
 
     public static ItemStack randomGetOne() {
