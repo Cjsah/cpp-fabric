95a23a147eabecb1e941ee4fb2296e15c648d3ea
Cjsah <2684117397@qq.com>
2020-12-25 00:56:33 +0800

    log欢迎

--- src/main/java/net/cpp/mixin/CraftingppMixin.java
+++ src/main/java/net/cpp/mixin/CraftingppMixin.java
@@ -1,13 +1,16 @@
 package net.cpp.mixin;
 
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.MinecraftClient;
+import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
+@Mixin(MinecraftClient.class)
 public class CraftingppMixin {
-    @Inject(at = @At("HEAD"), method = "init()V")
-    private void init(CallbackInfo info) {}
+    @Inject(at = @At("HEAD"), method = "run()V")
+    private void run(CallbackInfo info) {
+        LogManager.getLogger("Craftingpp").info("welcome to use cpp");
+    }
 }
