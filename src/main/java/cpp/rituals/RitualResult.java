58b1953444826db87d9ab3ca5fbf39595fc5dff5
꧁[C̲̅j̲̅s̲̅a̲̅h̲̅]꧂ <2684117397@qq.com>
2021-04-14 00:05:13 +0800

    优化一下

--- src/main/java/cpp/rituals/RitualResult.java
+++ src/main/java/cpp/rituals/RitualResult.java
@@ -2,9 +2,10 @@ package cpp.rituals;
 
 import cpp.ducktyping.ICppState;
 import cpp.init.CppItemTags;
-import cpp.state.CppStateOperate;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentLevelEntry;
+import net.minecraft.item.EnchantedBookItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -15,8 +16,9 @@ import net.minecraft.util.registry.Registry;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.List;
 
-public class RitualResult {
+public final class RitualResult {
 
     /**
      * @param rare1 稀有掉落物1
@@ -26,25 +28,13 @@ public class RitualResult {
     @Nonnull
     @SuppressWarnings("ConstantConditions")
     public static ItemStack getEnchantingResult(Item rare1, Item rare2) {
-        int index = 0;
-        ItemStack book = new ItemStack(Items.ENCHANTED_BOOK);
-        for (Item value1 : CppItemTags.RARE_DROPS.values()) {
-            for (Item value2 : CppItemTags.RARE_DROPS.values()) {
-                if (value1 == rare1 && value2 == rare2) {
-                    String enchantment = ((ICppState)MinecraftClient.getInstance().getServer()).getCppStateOperate().getEnchantment(index);
-                    CompoundTag compoundTag = new CompoundTag();
-                    ListTag listTag = new ListTag();
-                    CompoundTag enchantedTag = new CompoundTag();
-                    enchantedTag.putString("id", enchantment);
-                    enchantedTag.putShort("lvl", (short) Registry.ENCHANTMENT.get(new Identifier(enchantment)).getMaxLevel());
-                    listTag.add(enchantedTag);
-                    compoundTag.put("StoredEnchantments", listTag);
-                    book.setTag(compoundTag);
-                }
-                index++;
-            }
-        }
-        return book;
+        List<Item> rares = CppItemTags.RARE_DROPS.values();
+        int index = rares.indexOf(rare1) * rares.size() + rares.indexOf(rare2);
+        String enchantId = ((ICppState)MinecraftClient.getInstance().getServer()).getCppStateOperate().getEnchantment(index);
+        Enchantment enchantment = Registry.ENCHANTMENT.get(new Identifier(enchantId));
+        ItemStack enchantedBook = new ItemStack(Items.ENCHANTED_BOOK);
+        EnchantedBookItem.addEnchantment(enchantedBook, new EnchantmentLevelEntry(enchantment, enchantment.getMaxLevel()));
+        return enchantedBook;
     }
 
     /**
