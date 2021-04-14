26327c9999c06b3f07a95185d31cb33c3063e437
꧁[C̲̅j̲̅s̲̅a̲̅h̲̅]꧂ <2684117397@qq.com>
2021-04-14 18:07:23 +0800

    update

--- src/main/java/cpp/init/CppCommands.java
+++ src/main/java/cpp/init/CppCommands.java
@@ -9,7 +9,8 @@ public final class CppCommands {
     public static void register() {
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> dispatcher.register(
                 CommandManager.literal("cpp").then(CommandManager.literal("test").executes(commandContext -> {
-                    commandContext.getSource().getPlayer().giveItemStack(RitualResult.getEnchantingResult(CppItems.WING_OF_SKY, CppItems.SPIRIT_OF_LIFE));
+                    RitualResult.getEffectResult(CppItems.AGENTIA_OF_AGILENESS, CppItems.SPIRIT_OF_LIFE.getDefaultStack());
+//                    commandContext.getSource().getPlayer().giveItemStack(RitualResult.getEnchantingResult(CppItems.WING_OF_SKY, CppItems.SPIRIT_OF_LIFE));
                     return Command.SINGLE_SUCCESS;
                 }))
         ));
--- src/main/java/cpp/rituals/RitualResult.java
+++ src/main/java/cpp/rituals/RitualResult.java
@@ -1,16 +1,19 @@
 package cpp.rituals;
 
+import cpp.Craftingpp;
 import cpp.ducktyping.ICppState;
 import cpp.init.CppItemTags;
+import cpp.init.CppItems;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.item.EnchantedBookItem;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -32,7 +35,7 @@ public final class RitualResult {
         int index = rares.indexOf(rare1) * rares.size() + rares.indexOf(rare2);
         String enchantId = ((ICppState)MinecraftClient.getInstance().getServer()).getCppStateOperate().getEnchantment(index);
         Enchantment enchantment = Registry.ENCHANTMENT.get(new Identifier(enchantId));
-        ItemStack enchantedBook = new ItemStack(Items.ENCHANTED_BOOK);
+        ItemStack enchantedBook = Items.ENCHANTED_BOOK.getDefaultStack();
         EnchantedBookItem.addEnchantment(enchantedBook, new EnchantmentLevelEntry(enchantment, enchantment.getMaxLevel()));
         return enchantedBook;
     }
@@ -43,7 +46,20 @@ public final class RitualResult {
      * @return 附加效果后的装备
      */
     @Nonnull
-    protected static ItemStack getEffectResult(Item agentia, ItemStack item) {
+    @SuppressWarnings("ConstantConditions")
+    public static ItemStack getEffectResult(@Nonnull Item agentia, ItemStack item) {
+        FoodComponent foodComponent = agentia.getFoodComponent();
+        String tag;
+        if (foodComponent != null) {
+            StatusEffect effect = foodComponent.getStatusEffects().get(0).getFirst().getEffectType();
+            tag = Registry.STATUS_EFFECT.getId(effect).toString();
+        }else if (agentia == CppItems.MAGNET) {
+            tag = Craftingpp.MOD_ID3 + "magnet";
+        }else {
+            tag = "";
+        }
+        item.putSubTag("EquipmentEffect", StringTag.of(tag));
+
         return null;
     }
 
@@ -52,7 +68,7 @@ public final class RitualResult {
      * @return 附加属性后的装备
      */
     @Nonnull
-    protected static ItemStack getAttributeResult(ItemStack item) {
+    public static ItemStack getAttributeResult(ItemStack item) {
         return null;
     }
 
@@ -60,7 +76,7 @@ public final class RitualResult {
      * @return 刷怪笼
      */
     @Nonnull
-    protected static ItemStack getDarkResult() {
+    public static ItemStack getDarkResult() {
         return new ItemStack(Items.SPAWNER);
     }
 
@@ -68,11 +84,22 @@ public final class RitualResult {
      * @return 紫水晶母岩
      */
     @Nonnull
-    protected static ItemStack getLightResult() {
+    public static ItemStack getLightResult() {
         return new ItemStack(Items.AMETHYST_BLOCK);
     }
 
     public interface IRitualResult {
         ItemStack get(ItemStack input, @Nullable Item... items);
     }
+
+//    class EffectRecipes {
+//        private final Item agentia;
+//        private final StatusEffect effect;
+//        private final EquipmentSlot slot;
+//
+//        private EffectRecipes(Item agentia, StatusEffect) {
+//            EquipmentSlot.MAINHAND;
+//        }
+//
+//    }
 }
