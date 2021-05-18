ec50ffc6cd79a9bfc05ad257654a3812de1be3dd
Cjsah <2684117397@qq.com>
2021-05-18 16:16:24 +0800

    accessWidener

--- src/main/java/cpp/dark/DarkAnimalsTargetPredicate.java
+++ src/main/java/cpp/dark/DarkAnimalsTargetPredicate.java
@@ -9,6 +9,10 @@ import javax.annotation.Nullable;
 
 public class DarkAnimalsTargetPredicate extends TargetPredicate {
 
+    protected DarkAnimalsTargetPredicate() {
+        super(false);
+    }
+
     @Override
     public boolean test(@Nullable LivingEntity baseEntity, LivingEntity targetEntity) {
         if (targetEntity instanceof IPlayerVaccine && ((IPlayerVaccine) targetEntity).containVaccine(Vaccines.DARKNESS)) return false;
